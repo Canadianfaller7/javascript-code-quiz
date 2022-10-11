1c9ef7eb893bfbc05d84b093e4131a74db5363fa
Spencer Merrill <canadianfaller7@gmail.com>
2022-10-10 23:22:22 -0600

    Adding more funtionality to the quiz, trying to get answer choices to show on a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,154 @@
 /*
 
-    1. We will want to be able to make a button start a timer and hide the original html and start asking questions 
-    2. We will then need to answer the question and let the user know if the answer is right or wrong and if they are wrong then we will need to subtract some time from the overall timer. 
-    3. When all questions are answered
+1. We will want to be able to make a button start a timer and hide the original html and start asking questions 
+2. We will then need to answer the question and let the user know if the answer is right or wrong and if they are wrong then we will need to subtract some time from the overall timer. 
+3. When all questions are answered
 
 */
 
-let questions = 
-[
+const questions = [
     {
         question: "How do you write 'Hello World' in an alert box?",
-        buttons: ["msgBox('Hello World');","msg('Hello World');","alertBox('Hello World');","alert('Hello World');"],
-        answer: "alert('Hello World');"
-    },
-    {
-        question: "How do you create a function in JavaScript?",
-        buttons: ["function = myFunction()","function myFunction()","function:myFunction()","var function() = function"],
-        answer: "function myFunction()"
-    },
-    {
-        question: "How do you write an IF statement in JavaScript?",
-        buttons: ["if i = 5","if(i == 5)","if i == 5 then","if i = 5 then"],
-        answer: "if(i == 5)" 
-    },
-    {
-        question: "How does a FOR loop start?",
-        buttons: ["for (i = 0; i <= 5; i++)","for (i = 0; i <= 5)","for i = 1 to 5","for (i <= 5; i++)"],
-        answer: "for (i = 0; i &lt;= 5; i++)"
-    },
-    {
-        question: "Which event occurs when the user clicks on an HTML element?",
-        buttons: ["onmouseover","onmouseclick","onchange","onclick"],
-        answer: "onclick"
-    }
+        answers: [
+            { text: "alert('Hello World');", correct: true },
+            { text: "msgBox('Hello World');", correct: false},
+            { text: "msg('Hello World');", correct: false },
+            { text: "alertBox('Hello World');", correct: false }
+        ]
+    }//,
+    // {
+    //     question: "How do you create a function in JavaScript?",
+    //     answers: [
+    //         {
+    //             text:  "function = myFunction()", correct: false
+    //         },
+    //         {
+    //             text: "function myFunction()", correct: true
+    //         },
+    //         {
+    //             text: "function:myFunction()", correct: false
+    //         },
+    //         {
+    //             text: "var function() = function", correct: false
+    //         }
+    //     ]
+    // },
+    // {
+    //     question: "How do you write an IF statement in JavaScript?",
+    //     answers: [
+    //         {
+    //             text: "if i = 5", correct: false
+    //         },
+    //         {
+    //             text: "if(i === 5)", correct: true
+    //         },
+    //         {
+    //             text: "if i == 5 then", correct: false
+    //         },
+    //         {
+    //             text: "if i = 5 then", correct: false
+    //         },
+    //     ]
+    // },
+    // {
+    //     question: "How does a FOR loop start?",
+    //     answers: [
+    //         {
+    //             text: "for (let i = 0; i <= 5; i++)", correct: true
+    //         },
+    //         {
+    //             text: "for (i = 0; i <= 5)", correct: false
+    //         },
+    //         {
+    //             text: "for i = 1 to 5", correct: false
+    //         },
+    //         {
+    //             text: "for (i <= 5; i++)", correct: false
+    //         }
+    //     ]
+    // },
+    // {
+    //     question: "Which event occurs when the user clicks on an HTML element?",
+    //     answers: [
+    //         {
+    //             text: "onmouseover", correct: false
+    //         },
+    //         {
+    //             text: "onmouseclick", correct: false
+    //         },
+    //         {
+    //             text: "onchange", correct: false
+    //         },
+    //         {
+    //             text: "onclick", correct: true
+    //         }
+    //     ]
+    // }
 ];
 
 let highscore = [];
 let timeLeft = 60;
-let timeStart = false;
-
-let interval;
-
-let questionNum = 0;
 let score = 0;
-
-let questionTitle;
-let btn = [];
-let btnDiv = [];
-let btnHolder;
-
-let answerValidate;
-
-let card = document.getElementById('quiz-card');
 let timeEl = document.getElementById('time-left');
+let ranQuestion;
+let currentQuestionIndex;
+
+
+const startButton = document.getElementById('start-quiz');
+const questionData = document.getElementById('question-container')
+const mainText = document.getElementById('main-text');
+const questionEl = document.getElementById('question')
+const answerButtonsEl = document.getElementById('answer-btn')
+
 
 timeEl.innerHTML = `Time Left: ${timeLeft}`;
+startButton.addEventListener('click', startQuiz);
 
-const startButton = document.getElementById('start-quiz')
-
-startButton.addEventListener('click', startQuiz())
-
-const startQuiz = () => {
-    console.log('started quiz');
+function startQuiz() {
+    // score = 0;
+    mainText.classList.add('hide');
+    startButton.classList.add('hide');
+    ranQuestion = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionData.classList.remove('hide');
+    nextQuestion()
+    startTimer();
 }
 
-const setNextQuestion = () => {
-
+const nextQuestion = () => {
+    getQuestion(ranQuestion[currentQuestionIndex])
 }
 
-const selectAnswer = () => {
+const getQuestion = questionQ => {
+    questionEl.innerText = questionQ.question
+    questionQ.answers.forEach(answer => {
+        const buttons = document.createElement('button')
+        buttons.innerText = answer.text 
+        buttons.classList.add('btn')
+        if (answer.correct) {
+            buttons.dataset.correct = answer.correct
+        }
+        buttons.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(buttons)
+    })
+}
 
+const selectAnswer = e => {
+    console.log('object');
+}
+
+function startTimer() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timeEl.textContent = `Time Left: ${timeLeft}`;
+        
+        if(timeLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            
+            return;
+        }
+        
+    }, 1000);
 }
